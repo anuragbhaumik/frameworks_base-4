18c70d497ff8695dce35cdefdd1d1b150f2e32ba
TikkiTikki <radstylemusic@gmail.com>
2021-07-03 23:51:50 +0200

    base: Add Ambient Text Customizations [1/2]
    This adds the option to add custom text on the ambient screen.
    
    [nurkeinneid]:
    - port to T
    - stripped down version - only custom text
    
    Change-Id: I8aa8cdeb0b55783a18abcfecb068907f4bc2122b
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -117,11 +117,15 @@ import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
 import android.view.accessibility.AccessibilityManager;
 import android.widget.DateTimeView;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleRegistry;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.colorextraction.ColorExtractor;
@@ -161,6 +165,7 @@ import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dagger.qualifiers.UiBackground;
 import com.android.systemui.demomode.DemoMode;
 import com.android.systemui.demomode.DemoModeController;
+import com.android.systemui.derp.ambient.AmbientController;
 import com.android.systemui.dreams.DreamOverlayStateController;
 import com.android.systemui.emergency.EmergencyGesture;
 import com.android.systemui.flags.FeatureFlags;
@@ -677,6 +682,10 @@ public class CentralSurfacesImpl extends CoreStartable implements
     private boolean mWallpaperSupported;
 
     private VisualizerView mVisualizerView;
+    private AmbientController mAmbientController;
+    private TextView mAmbientTextView;
+    private FrameLayout mAmbientContainer;
+    private ConstraintLayout mAmbientTextContainer;
 
     private Runnable mLaunchTransitionEndRunnable;
     private Runnable mLaunchTransitionCancelRunnable;
@@ -920,6 +929,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
         mTunerService = tunerService;
         mSysUiState = sysUiState;
         mPulseController = new PulseControllerImpl(mContext, this, mCommandQueue, mUiBgExecutor);
+        mAmbientController = new AmbientController(context, mAmbientContainer, mAmbientTextContainer, mAmbientTextView, mNotificationPanelViewController);
 
         mLockscreenShadeTransitionController = lockscreenShadeTransitionController;
         mStartingSurfaceOptional = startingSurfaceOptional;
@@ -1396,6 +1406,9 @@ public class CentralSurfacesImpl extends CoreStartable implements
         }
 
         mVisualizerView = (VisualizerView) mNotificationShadeWindowView.findViewById(R.id.visualizerview);
+        mAmbientContainer = mNotificationShadeWindowView.findViewById(R.id.ambient_container);
+        mAmbientTextContainer = mNotificationShadeWindowView.findViewById(R.id.ambient_text_container);
+        mAmbientTextView = mNotificationShadeWindowView.findViewById(R.id.ambient_text);
 
         mReportRejectedTouch = mNotificationShadeWindowView
                 .findViewById(R.id.report_rejected_touch);
@@ -3625,6 +3638,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
 
         mNotificationPanelViewController.setDozing(mDozing, animate, mWakeUpTouchLocation);
         mPulseController.setDozing(mDozing);
+        mAmbientController.setDozing(mDozing);
         updateQsExpansionEnabled();
         Trace.endSection();
     }
@@ -4950,6 +4964,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
                     updateScrimController();
                     mPresenter.updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
                     mPulseController.setKeyguardShowing(mState == StatusBarState.KEYGUARD);
+                    mAmbientController.setKeyguardShowing(mState == StatusBarState.KEYGUARD);
                     Trace.endSection();
                 }
 
@@ -4998,6 +5013,11 @@ public class CentralSurfacesImpl extends CoreStartable implements
 
     public VisualizerView getLsVisualizer() {
         return mVisualizerView;
+    }
+
+
+    public AmbientController getAmbientController() {
+        return mAmbientController;
     }
 
     private final BatteryController.BatteryStateChangeCallback mBatteryStateChangeCallback =
