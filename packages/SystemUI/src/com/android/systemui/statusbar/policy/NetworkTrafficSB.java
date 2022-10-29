Project-Amatsuka/frameworks_base

fw/b: Squash import NetworkTraffic
This is a squash of the following:

Author: Jon Haus <haus.xda@gmail.com>
Date:   Thu Jan 5 00:20:06 2017 +0100

Network Traffic [1/2]

-----
abc ezio84:
use O darkintensity api
use arrows vectors from aosp net arrows commit (thanks @Mazda-- for this find!): https://github.com/ezio84/abc_frameworks_base/commit/ad71ae1fd09f9a7392ab2eba7b766c8b088319b4
fix visual glitch on statusbar expand (arrows showing and quickly hiding again)
don't show arrows when no data
remove unit settings (KB is good)
remove interval (set to 1sec by def) and single arrow options
don't get settings values again when not needed
fix padding between arrows and speed values and expose the padding value
code cleanup
-----

Change-Id: I026f67caec69a09c0c1969a09fbe99aa6b6f0510

Author: ezio84 <brabus84@gmail.com>
Date:   Sun Dec 9 20:17:58 2018 +0100

    Move net monitor to expanded statusbar header

    Preview: https://i.ibb.co/1TCCwV8/455010636-14778.jpg

    Actually, with notch or low dpi and more statusbar icons we have not so
    much space there to show the net monitor.
    A notch can hide it and the system can now handle only a fixed max number of
    notifications and status icons, so when net monitor shows itself it can force
    the system to drop some icons to show the infamous dot.

    Let's also consider we usually check the monitor to see if the data
    transfer is working as expected and the speed is proper, so it's not
    an info we want to see always (it's also distracting when reading
    something).

    So, let's move it to expanded statusbar.
    There we have also some room to add line spacing and more info.

    Change-Id: I3fca786aec4f27c763e70d404448bd71dfaecceb

Author: SpiritCroc <dev@spiritcroc.de>
Date:   Mon Dec 24 15:03:44 2018 +0100

    Network traffic: fix indicator not hiding on lost connection

    With https://gerrit.aicp-rom.com/c/AICP/frameworks_base/+/80636
    ("Statusbar net monitor: stop the handler if screen is off

packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -1,0 +1,155 @@
+package com.android.systemui.statusbar.policy;
+
+import static com.android.systemui.statusbar.StatusBarIconView.STATE_DOT;
+import static com.android.systemui.statusbar.StatusBarIconView.STATE_HIDDEN;
+import static com.android.systemui.statusbar.StatusBarIconView.STATE_ICON;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.text.style.RelativeSizeSpan;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.plugins.DarkIconDispatcher;
+import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
+import com.android.systemui.statusbar.StatusIconDisplayable;
+
+public class NetworkTrafficSB extends NetworkTraffic implements DarkReceiver, StatusIconDisplayable {
+
+    public static final String SLOT = "networktraffic";
+    private int mVisibleState = -1;
+    private boolean mSystemIconVisible = true;
+
+    /*
+     *  @hide
+     */
+    public NetworkTrafficSB(Context context) {
+        this(context, null);
+    }
+
+    /*
+     *  @hide
+     */
+    public NetworkTrafficSB(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    /*
+     *  @hide
+     */
+    public NetworkTrafficSB(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    protected void setMode() {
+        super.setMode();
+        mIsEnabled = mIsEnabled;
+    }
+
+    @Override
+    protected void setSpacingAndFonts() {
+        setTypeface(Typeface.create("sans-serif-condensed", Typeface.BOLD));
+        setLineSpacing(0.75f, 0.75f);
+    }
+
+    @Override
+    protected RelativeSizeSpan getSpeedRelativeSizeSpan() {
+        return new RelativeSizeSpan(0.75f);
+    }
+
+    @Override
+    protected RelativeSizeSpan getUnitRelativeSizeSpan() {
+        return new RelativeSizeSpan(0.7f);
+    }
+
+    @Override
+    public void onDarkChanged(Rect area, float darkIntensity, int tint) {
+        if (!mIsEnabled) return;
+        mTintColor = DarkIconDispatcher.getTint(area, this, tint);
+        setTextColor(mTintColor);
+        updateTrafficDrawable();
+    }
+
+    @Override
+    public String getSlot() {
+        return SLOT;
+    }
+
+    @Override
+    public boolean isIconVisible() {
+        return mIsEnabled;
+    }
+
+    @Override
+    public int getVisibleState() {
+        return mVisibleState;
+    }
+
+    @Override
+    public void setVisibleState(int state, boolean mIsEnabled) {
+        if (state == mVisibleState) {
+            return;
+        }
+        mVisibleState = state;
+
+        switch (state) {
+            case STATE_ICON:
+                mSystemIconVisible = true;
+                break;
+            case STATE_DOT:
+            case STATE_HIDDEN:
+            default:
+                mSystemIconVisible = false;
+                break;
+        }
+        update();
+    }
+
+    @Override
+    protected void makeVisible() {
+        boolean show = mSystemIconVisible;
+        setVisibility(show ? View.VISIBLE
+                : View.GONE);
+        mVisible = show;
+    }
+
+    @Override
+    public void setStaticDrawableColor(int color) {
+        mTintColor = color;
+        setTextColor(mTintColor);
+        updateTrafficDrawable();
+    }
+
+    @Override
+    public void setDecorColor(int color) {
+    }
+
+    private void maybeRestoreVisibility() {
+        if (!mVisible && mIsEnabled && mSystemIconVisible
+           && restoreViewQuickly()) {
+          setVisibility(View.VISIBLE);
+          mVisible = true;
+          // then let the traffic handler do its checks
+          update();
+        }
+    }
+
+    public void setTintColor(int color) {
+        mTintColor = color;
+        updateTrafficDrawable();
+    }
+}

anuragbhaumik/frameworks_base-4

packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -1,0 +1,157 @@
+package com.android.systemui.statusbar.policy;
+
+import static com.android.systemui.statusbar.StatusBarIconView.STATE_DOT;
+import static com.android.systemui.statusbar.StatusBarIconView.STATE_HIDDEN;
+import static com.android.systemui.statusbar.StatusBarIconView.STATE_ICON;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.text.style.RelativeSizeSpan;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.plugins.DarkIconDispatcher;
+import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
+import com.android.systemui.statusbar.StatusIconDisplayable;
+
+import java.util.ArrayList;
+
+public class NetworkTrafficSB extends NetworkTraffic implements DarkReceiver, StatusIconDisplayable {
+
+    public static final String SLOT = "networktraffic";
+    private int mVisibleState = -1;
+    private boolean mSystemIconVisible = true;
+
+    /*
+     *  @hide
+     */
+    public NetworkTrafficSB(Context context) {
+        this(context, null);
+    }
+
+    /*
+     *  @hide
+     */
+    public NetworkTrafficSB(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    /*
+     *  @hide
+     */
+    public NetworkTrafficSB(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    protected void setMode() {
+        super.setMode();
+        mIsEnabled = mIsEnabled;
+    }
+
+    @Override
+    protected void setSpacingAndFonts() {
+        setTypeface(Typeface.create("sans-serif-condensed", Typeface.BOLD));
+        setLineSpacing(0.75f, 0.75f);
+    }
+
+    @Override
+    protected RelativeSizeSpan getSpeedRelativeSizeSpan() {
+        return new RelativeSizeSpan(0.75f);
+    }
+
+    @Override
+    protected RelativeSizeSpan getUnitRelativeSizeSpan() {
+        return new RelativeSizeSpan(0.7f);
+    }
+
+    @Override
+    public void onDarkChanged(ArrayList<Rect> areas, float darkIntensity, int tint) {
+        if (!mIsEnabled) return;
+        mTintColor = DarkIconDispatcher.getTint(areas, this, tint);
+        setTextColor(mTintColor);
+        updateTrafficDrawable();
+    }
+
+    @Override
+    public String getSlot() {
+        return SLOT;
+    }
+
+    @Override
+    public boolean isIconVisible() {
+        return mIsEnabled;
+    }
+
+    @Override
+    public int getVisibleState() {
+        return mVisibleState;
+    }
+
+    @Override
+    public void setVisibleState(int state, boolean mIsEnabled) {
+        if (state == mVisibleState) {
+            return;
+        }
+        mVisibleState = state;
+
+        switch (state) {
+            case STATE_ICON:
+                mSystemIconVisible = true;
+                break;
+            case STATE_DOT:
+            case STATE_HIDDEN:
+            default:
+                mSystemIconVisible = false;
+                break;
+        }
+        update();
+    }
+
+    @Override
+    protected void makeVisible() {
+        boolean show = mSystemIconVisible;
+        setVisibility(show ? View.VISIBLE
+                : View.GONE);
+        mVisible = show;
+    }
+
+    @Override
+    public void setStaticDrawableColor(int color) {
+        mTintColor = color;
+        setTextColor(mTintColor);
+        updateTrafficDrawable();
+    }
+
+    @Override
+    public void setDecorColor(int color) {
+    }
+
+    private void maybeRestoreVisibility() {
+        if (!mVisible && mIsEnabled && mSystemIconVisible
+           && restoreViewQuickly()) {
+          setVisibility(View.VISIBLE);
+          mVisible = true;
+          // then let the traffic handler do its checks
+          update();
+        }
+    }
+
+    public void setTintColor(int color) {
+        mTintColor = color;
+        updateTrafficDrawable();
+    }
+}