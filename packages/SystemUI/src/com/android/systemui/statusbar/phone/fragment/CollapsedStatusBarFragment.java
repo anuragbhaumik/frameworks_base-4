crdroidandroid/android_frameworks_base

SystemUI: Fix clock padding with clock background chip
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -395,12 +395,20 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             mRightClockView.setBackgroundResource(R.drawable.sb_date_bg);
             mRightClockView.setPadding(10,2,10,2);
         } else {
+            int clockPaddingStart = getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_clock_starting_padding);
+            int clockPaddingEnd = getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_clock_end_padding);
+            int leftClockPaddingStart = getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_left_clock_starting_padding);
+            int leftClockPaddingEnd = getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_left_clock_end_padding);
             mClockView.setBackgroundResource(0);
-            mClockView.setPadding(0,0,0,0);
+            mClockView.setPadding(leftClockPaddingStart, 0, leftClockPaddingEnd, 0);
             mCenterClockView.setBackgroundResource(0);
             mCenterClockView.setPadding(0,0,0,0);
             mRightClockView.setBackgroundResource(0);
-            mRightClockView.setPadding(0,0,0,0);
+            mRightClockView.setPadding(clockPaddingStart, 0, clockPaddingEnd, 0);
         }
     }
 

anuragbhaumik/frameworks_base-4

packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -747,12 +747,20 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             mRightClock.setBackgroundResource(R.drawable.sb_date_bg);
             mRightClock.setPadding(10,5,10,5);
         } else {
+            int clockPaddingStart = getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_clock_starting_padding);
+            int clockPaddingEnd = getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_clock_end_padding);
+            int leftClockPaddingStart = getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_left_clock_starting_padding);
+            int leftClockPaddingEnd = getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_left_clock_end_padding);
             mLeftClock.setBackgroundResource(0);
-            mLeftClock.setPadding(0,0,0,0);
+            mLeftClock.setPadding(leftClockPaddingStart, 0, leftClockPaddingEnd, 0);
             mCenterClock.setBackgroundResource(0);
             mCenterClock.setPadding(0,0,0,0);
             mRightClock.setBackgroundResource(0);
-            mRightClock.setPadding(0,0,0,0);
+            mRightClock.setPadding(clockPaddingStart, 0, clockPaddingEnd, 0);
         }
     }
 }