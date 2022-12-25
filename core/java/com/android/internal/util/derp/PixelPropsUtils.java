crdroidandroid/android_frameworks_base

PixelPropsUtils: Remove spoofing for currently supported Pixel devices
@neobuddy89: Selectively enabled for GPhotos spoof.

Change-Id: I3a426f8671f841c16e5af7c0a2a204d19c502464
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -31,6 +31,7 @@ import java.util.Map;
 public class PixelPropsUtils {
 
     private static final String TAG = PixelPropsUtils.class.getSimpleName();
+    private static final String DEVICE = "ro.product.device";
     private static final boolean DEBUG = false;
 
     private static final Map<String, Object> propsToChange;
@@ -118,6 +119,20 @@ public class PixelPropsUtils {
             "com.tencent.tmgp.sgame"
     };
 
+    // Codenames for currently supported Pixels by Google
+    private static final String[] pixelCodenames = {
+            "oriole",
+            "raven",
+            "redfin",
+            "barbet",
+            "bramble",
+            "sunfish",
+            "coral",
+            "flame",
+            "bonito",
+            "sargo"
+    };
+
     private static volatile boolean sIsGms = false;
 
     static {
@@ -171,13 +186,17 @@ public class PixelPropsUtils {
         if (packageName.startsWith("com.google.")
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
 
+            boolean isPixelDevice = Arrays.asList(pixelCodenames).contains(SystemProperties.get(DEVICE));
+
             if (packageName.equals("com.google.android.apps.photos")) {
                 if (SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
                     propsToChange.putAll(propsToChangePixelXL);
                 } else {
+                    if (isPixelDevice) return;
                     propsToChange.putAll(propsToChangePixel5);
                 }
             } else {
+                if (isPixelDevice) return;
                 if (Arrays.asList(packagesToChangePixel6).contains(packageName)) {
                     propsToChange.putAll(propsToChangePixel6);
                 } else if (Arrays.asList(packagesToChangePixelXL).contains(packageName)) {

anuragbhaumik/frameworks_base-4

core/java/com/android/internal/util/derp/PixelPropsUtils.java
@@ -18,6 +18,7 @@
 package com.android.internal.util.derp;
 
 import android.os.Build;
+import android.os.SystemProperties;
 import android.util.Log;
 
 import java.lang.reflect.Field;
@@ -30,6 +31,7 @@ import java.util.Map;
 public class PixelPropsUtils {
 
     private static final String TAG = PixelPropsUtils.class.getSimpleName();
+    private static final String DEVICE = "ro.derp.device";
     private static final boolean DEBUG = false;
 
     private static final Map<String, Object> propsToChange;
@@ -80,6 +82,21 @@ public class PixelPropsUtils {
         "com.google.android.apps.youtube.music"
     };
 
+    // Codenames for currently supported Pixels by Google
+    private static final String[] pixelCodenames = {
+            "cheetah",
+            "panther",
+            "bluejay",
+            "oriole",
+            "raven",
+            "barbet",
+            "redfin",
+            "bramble",
+            "sunfish",
+            "coral",
+            "flame"
+    };
+
     private static volatile boolean sIsGms = false;
 
     static {
@@ -119,9 +136,11 @@ public class PixelPropsUtils {
         if (packageName.startsWith("com.google.")
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
 
+            boolean isPixelDevice = Arrays.asList(pixelCodenames).contains(SystemProperties.get(DEVICE));
+
             if (packageName.equals("com.google.android.apps.photos")) {
                 propsToChange.putAll(propsToChangePixelXL);
-            } else {
+            } else if (!isPixelDevice) {
                 if (Arrays.asList(packagesToChangePixel6).contains(packageName)) {
                     propsToChange.putAll(propsToChangePixel6);
                 } else if (Arrays.asList(packagesToChangePixelXL).contains(packageName)) {